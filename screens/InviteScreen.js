aacba887403272459e56958f231bd478baef8a01
MaryAnne45 <mary.jereh@gmail.com>
2019-04-09 18:42:33 +0300

    displaying a list of events that you can swipe right or left to accept or decline

--- screens/InviteScreen.js
+++ screens/InviteScreen.js
@@ -1,18 +1,58 @@
 import React from  'react'; 
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet,ListView} from 'react-native';
+import { Container,Header,Content,Button,Icon,List,ListItem,Text,Toast} from 'native-base'
+import { randomUI12 } from 'uuid-js';
+const events = [
+    'Otis Schinner',
+    'Domingo Streich',
+    'Prof. Michelle Muller IV',
+    'Charlie Dibbert DDS',
+    'Ashlynn Gorczany',
+    'Domingo Streich',
+    'Swimming with the Dolphins',
+    'Hiking',
 
+]
 export default class Friends extends  React.Component {
+    constructor(props){
+        super(props);
+        this.ds = new ListView.DataSource({rowHasChanged:(r1,r2)=>r1 !==r2});
+        this.state = {
+            basic: true,
+            ListViewData: events
+        };
+
+    }
+    deleteRow(secId,rowId,rowMap){
+        rowMap[`${secId}${rowId}`].props.closeRow();
+        const newData=[...this.state.ListViewData];
+        newData.splice(rowId,1);
+        this.setState({ListViewData:newData})
+    }
 render() {
     return(
-        /*
-        the this.props.navigation as long as  the screen 
-        is included in stacknavigato it inherits  many useful props from the navigation 
-        object 
-        */
-        <View style={styles.container}>
-        <Text>Invite.....</Text>
-        
-        </View>
+       
+       <Container>
+           <Content padder>
+               <List 
+               leftOpenValue ={75}
+               rightOpenValue={-75}
+               dataSource={this.ds.cloneWithRows(this.state.ListViewData)}
+               renderRow={event =>
+                <ListItem>
+                    <Text> {event}</Text>
+                </ListItem>} 
+                renderLeftHiddenRow={event=>
+                 <Button success full onPress={()=>alert('Invite Accepted') }>
+                    <Icon active name="md-checkmark-circle-outline"/>
+                </Button>} 
+                renderRightHiddenRow={event=>
+                    <Button danger full onPress={()=>alert('Invite Declined')}>
+                   <Icon active name="md-close-circle-outline"/>
+                    </Button>}
+                />
+               </Content>
+        </Container>
     );
 }
 } 
