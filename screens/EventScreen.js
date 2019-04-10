5ece5968f6e30eb925e32c542243aba6093abda2
MaryAnne45 <mary.jereh@gmail.com>
2019-04-10 16:58:19 +0300

    changed the events page design

--- screens/EventScreen.js
+++ screens/EventScreen.js
@@ -13,7 +13,7 @@ const events = [
   events_notes: 'Saepe impedit asperiores ea consecteturConsequuntur aut neque voluptatem quisquam et ipsa. Atque et et facilis illo pariatur qui ea. Qui neque fugiat aperiam dolore repellat',
   Team_manager_notes:'',
   staff:'Adan Hodkiewicz',
-  icon:'',
+  url:'https://images.unsplash.com/photo-1553532070-2ca3b5876896?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
 
 },
 { 
@@ -28,7 +28,8 @@ const events = [
   second_date:'Saturday April 13th 2019 at 11:27 am',
   events_notes: 'Sit ad adipisci ab et nihil. Et architecto itaque consequatur eos odio dolorum odit. ',
   Team_manager_notes:'Sit sint et doloribus magni sed neque voluptatem excepturi. ',
-  staff:'Lauren Jaskolski'
+  staff:'Lauren Jaskolski',
+  url:'https://images.unsplash.com/photo-1554631492-a054feeae929?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80'
 },
 {  
     id: 3,
@@ -42,7 +43,8 @@ const events = [
     second_date:'Thursday April 11th 2019 at 6:27 pm',
     events_notes: '',
     Team_manager_notes:'Facilis assumenda maxime delectus est. Eaque minima nesciunt saepe ',
-    staff:'Darren Koss'
+    staff:'Darren Koss',
+    url:'https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
   },
   {   
       id: 4,
@@ -56,7 +58,8 @@ const events = [
       second_date:'Friday April 12th 2019 at 7:27 pm',
       events_notes: 'Culpa tenetur labore est necessitatibus dicta consequatur suscipit dicta.',
       Team_manager_notes:'Optio molestias rem consequuntur hic. Debitis quia quisquam voluptas ',
-      staff:'Bruce Wilderman'
+      staff:'Bruce Wilderman',
+      url:'https://images.unsplash.com/photo-1519916755070-61536165b3d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
     },
     {   
        id: 5,
@@ -70,7 +73,8 @@ const events = [
         second_date:'Thursday April 11th 2019 at 2:27 pm',
         events_notes: 'Culpa tenetur labore est necessitatibus dicta consequatur suscipit dicta.',
         Team_manager_notes:'Optio molestias rem consequuntur hic. Debitis quia quisquam voluptas',
-        staff:'Abner Becker'
+        staff:'Abner Becker',
+        url:'https://images.unsplash.com/photo-1512053459797-38c3a066cabd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
       },
       { 
         id: 6,
@@ -84,21 +88,15 @@ const events = [
         second_date:'Saturday April 13th 2019 at 11:27 am',
         events_notes: 'Sit ad adipisci ab et nihil. Et architecto itaque consequatur eos odio dolorum odit. ',
         Team_manager_notes:'Sit sint et doloribus magni sed neque voluptatem excepturi. ',
-        staff:'Lauren Jaskolski'
+        staff:'Lauren Jaskolski',
+        url:'https://images.unsplash.com/photo-1511192336575-5a79af67a629?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80',
       },
 ]
 
 import React from 'react';
-import { StyleSheet, Text, View ,Button,Image,ScrollView,TouchableWithoutFeedback,Animated,Easing } from 'react-native';
-import { Card,Icon } from 'react-native-elements';
+import { StyleSheet} from 'react-native';
+import {Container,Content,Thumbnail,List,ListItem,Left, Body,Right,Text,Icon,Header,Tab,Tabs,TabHeading} from 'native-base';
 import { Font } from 'expo'; 
-import Header from '../components/Header'; 
-
-let scaleValue = new Animated.Value(0)
-const cardScale = scaleValue.interpolate({
-  inputRange: [0, 0.5, 1],
-  outputRange: [1, 1.1, 1.2]
-});
 
 
 export default class EventScreen extends React.Component {
@@ -109,80 +107,53 @@ export default class EventScreen extends React.Component {
     // await AsyncStorage.clear();
     this.props.navigation.navigate('Auth');
   }
- 
-_eventDetails=()=>{ 
-  scaleValue.setValue(0);
-  Animated.timing(scaleValue,{
-    toValue:1,
-    duration:250,
-    easing:Easing.linear,
-    useNativeDriver: true
-  }).start()
-  cardAction();
-  
-} 
-  render() {   
-  
- 
-    let transformStyle =  { ...styles.card, transform: [{ scale: cardScale }] };
-    let icon = {
-      url:"https://img.icons8.com/ultraviolet/80/000000/overtime.png"
+ render() {   
+  let icon = {
+      url:"https://img.icons8.com/bubbles/50/000000/calendar.png"
   }
    
-    return (
-      <ScrollView> 
-        <TouchableWithoutFeedback 
-        onPressIn={()=>{
-          scaleValue.setValue(0);
-          Animated.timing(scaleValue, {
-            toValue: 1,
-            duration: 250,
-            easing: Easing.linear,
-            useNativeDriver: true
-          }).start(); 
-          this.props.navigation.navigate('EventDetails',{data:events});
+    return ( 
+      
+     <Container>
+       <Content> 
+         
+         <Tabs>
+           <Tab heading={<TabHeading><Text>All</Text></TabHeading>}>
+           </Tab> 
+           <Tab heading={<TabHeading><Text>Today</Text></TabHeading>}>
+           </Tab>
+           <Tab heading={<TabHeading><Text>Tomorrow</Text></TabHeading>}>
+           </Tab> 
+           <Tab heading={<TabHeading><Text>This Week</Text></TabHeading>}>
+           </Tab>
+           </Tabs>
+         {
+         events.map((event,i) => {
+           return (
+          <List key={i}> 
+          <ListItem thumbnail>
+        <Left>
+        <Thumbnail square source={{uri:event.url}} />
+        </Left>
+        <Body>
+          <Text>{event.event}</Text>
+          <Text note>{event.first_date} GMT+</Text>
+          <Text note>{event.venue}</Text> 
+
+
+          </Body>
+        <Right>
+        <Icon active name="arrow-forward" />
           
-        }} 
-        onPressOut={() => {
-          Animated.timing(scaleValue, {
-            toValue: 0,
-            duration: 100,
-            easing: Easing.linear,
-            useNativeDriver: true
-          }).start();
-        }}> 
-        
+        </Right>
+        </ListItem>
+        </List>
+           );
+         })
+        }
+          </Content>
+       </Container>
        
-       
-            <Animated.View style={transformStyle}>
-            {
-          events.map((event, key) => {
-           return ( 
-            <Card containerStyle={styles.containerStyle} key={key}>
-            
-            <View style={styles.row}>
-            <Image resizeMode="contain" source={icon} style={styles.logo}/>
-            <Text style={styles.card_text}>{event.event}</Text>
-            
-            
-            </View> 
-            <View style={styles.direction}>
-            <Text style={{fontSize: 10,textAlign: 'center',}}> {event.Time}</Text> 
-            <Text style={{fontSize: 10,textAlign: 'center',}}> {event.venue}</Text>
-           </View>
-          
-            </Card>
-              );
-            })
-          }
-            </Animated.View>
-          
-     
-        
-       </TouchableWithoutFeedback>
-        <Button title="Actually, sign me out :)" onPress={this._signOutAsync} /> 
-       
-      </ScrollView>
     )
       
   }
@@ -196,7 +167,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   card_text :{
-    alignItems: 'center',
+    fontSize: 15,
   },
   containerStyle:{
     padding: 0, 
@@ -211,7 +182,7 @@ const styles = StyleSheet.create({
     elevation: 6,
    },
    logo: {
-    width: 90,
+    width: 70,
     height: 40,
     marginRight:0
   },
