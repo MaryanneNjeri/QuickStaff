26be4bb21e51b5f9f87417474e28cf3d2e565c3a
MaryAnne45 <mary.jereh@gmail.com>
2019-04-10 13:59:47 +0300

    added a settings screen with a logout option

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,17 +1,99 @@
 import React from  'react'; 
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet,Text } from 'react-native';
+import {Container,List,Button,Icon,Body, ListItem,Content,Left,Right,Switch} from 'native-base'
 
 export default class Friends extends  React.Component {
+    logout=()=>{
+        this.props.navigation.navigate('Auth');
+    }
 render() {
     return(
-        /*
-        the this.props.navigation as long as  the screen 
-        is included in stacknavigato it inherits  many useful props from the navigation 
-        object 
-        */
-        <View style={styles.container}>
-        <Text>Settings</Text>
-        </View>
+        <Container>
+            <Content>
+   
+               <ListItem icon> 
+               <Left>
+                   <Icon name="ios-notifications" />
+                   </Left> 
+                   <Body>
+                   <Text>Notifications</Text>
+                   </Body> 
+                   <Right>
+                       <Switch value={true}/>
+                    </Right>
+                   </ListItem> 
+
+                   <ListItem icon> 
+               <Left>
+                   <Icon name="person" />
+                   </Left> 
+                   <Body>
+                   <Text>Account</Text>
+                   </Body> 
+                   <Right>
+                   <Icon active name="arrow-forward" />
+                    </Right>
+                   </ListItem> 
+                   <ListItem icon> 
+                  <Left>
+                   <Icon name="ios-paper"/>
+                   </Left> 
+                   <Body>
+                   <Text>Edit Info</Text>
+                   </Body> 
+                   <Right>
+                   <Icon active name="arrow-forward" />
+                    </Right>
+                   </ListItem> 
+                   <ListItem icon> 
+                  <Left>
+                   <Icon name="ios-calendar" />
+                   </Left> 
+                   <Body>
+                   <Text>Calendar</Text>
+                   </Body> 
+                   <Right>
+                   <Icon active name="arrow-forward" />
+                    </Right>
+                   </ListItem>
+                   <ListItem icon> 
+                  <Left>
+                   <Icon type="Entypo" name="location-pin" />
+                   </Left> 
+                   <Body>
+                   <Text>Location </Text>
+                   </Body> 
+                   <Right>
+                   <Icon active name="arrow-forward" />
+                    </Right>
+                   </ListItem>
+                   <ListItem icon> 
+                  <Left>
+                   <Icon name="ios-help-circle-outline" />
+                   </Left> 
+                   <Body>
+                   <Text>Help</Text>
+                   </Body> 
+                   <Right>
+                   <Icon active name="arrow-forward" />
+                    </Right>
+                   </ListItem> 
+                   <ListItem icon> 
+                  <Left>
+                   <Icon  type="Feather" name="power" onPress={this.logout} />
+                   </Left> 
+                   <Body>
+                   <Text>Logout</Text>
+                   </Body> 
+                   <Right>
+                   <Icon active name="arrow-forward" />
+                    </Right>
+                   </ListItem> 
+                   
+               
+               </Content>
+        </Container>
+       
     );
 }
 } 
