476ae49e47aa1bfecfc8fdc1393532d86793c057
maryanne <mary.jereh@gmail.com>
2019-05-27 15:10:32 +0300

    rendering modal as a seperate component

--- screens/EventDetailsScreen.js
+++ screens/EventDetailsScreen.js
@@ -18,7 +18,7 @@ import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import {store} from '../Redux/store';
 import Geocoder from 'react-native-geocoding';
 import moment from 'moment';
-
+import AddEventModal from '../components/AddEventModal';
 const _ = require('lodash');
 const {width} = Dimensions.get('window');
 
@@ -39,6 +39,7 @@ export default class EventDetailsScreen extends React.Component {
 
     componentDidMount() {
         this.getEventDetails()
+
 
     }
 
@@ -92,104 +93,22 @@ export default class EventDetailsScreen extends React.Component {
         this.setState({
             isModalVisible: visible
         })
-    }
+    };
+    // we use a call back function to pass he props from child to the parent function
+    closeModal =()=>{
+        this.setState({
+            isModalVisible: false
+        })
 
+    }
     render() {
+
         return (
             <Container style={styles.container}>
                 <Content>
+                    {/*    we only render this component if  the isModalVisible has been set to true */}
+                    {this.state.isModalVisible ? <AddEventModal {...this.state} closeModal={this.closeModal}/>:null}
 
-                    <Modal visible={this.state.isModalVisible}
-                           animationType="slide"
-                           transparent={false}>
-
-                        <Content>
-
-                            <View style={{marginTop: 22}}>
-                                <TouchableHighlight
-                                    style={{alignSelf: "flex-end"}}
-                                    onPress={() => {
-                                        this.setModalVisible(!this.state.isModalVisible)
-                                    }}
-                                >
-                                    <Text style={{fontWeight: '200', color: '#303B43', fontSize: 13}}> Close<Icon
-                                        name="close" type="EvilIcons" style={{fontSize: 20, color: '#303B43'}}/></Text>
-                                </TouchableHighlight>
-                                <Body>
-
-                                    <Icon type="AntDesign" name="exclamationcircle"
-                                          style={{fontSize: 100, color: '#0052cc', textAlign: "center"}}/>
-                                    <Text style={{fontSize: 20, fontWeight: "200", color: '#303B43'}}>Add Event to
-                                        Calendar</Text>
-                                </Body>
-                                <View style={{padding: 18}}>
-                                    <Form>
-                                        <Item floatingLabel>
-                                            <Icon type="SimpleLineIcons" name="event"
-                                                  style={{fontSize: 15, color: '#303B43'}}/>
-                                            <Label style={{color: '#303B43', fontSize: 10}}>Event Name</Label>
-                                            <Input
-                                                style={{fontSize: 15}}
-
-
-                                                onChangeText={(event_name) => this.setState({event_name})}
-                                            />
-                                        </Item>
-                                        <Item floatingLabel>
-                                            <Icon type="Ionicons" name="md-people"
-                                                  style={{fontSize: 15, color: '#303B43'}}/>
-                                            <Label style={{color: '#303B43', fontSize: 10}}>Client</Label>
-                                            <Input
-                                                style={{fontSize: 15}}
-                                                onChangeText={(client) => this.setState({client})}
-                                            />
-                                        </Item>
-
-
-                                        <Item floatingLabel>
-                                            <Icon name="clock" type="EvilIcons"
-                                                  style={{fontSize: 15, color: '#303B43'}}/>
-                                            <Label style={{color: '#303B43', fontSize: 10}}>Starts at </Label>
-                                            <Input
-                                                style={{fontSize: 15}}
-                                                onChangeText={(starts_at) => this.setState({starts_at})}
-                                            />
-                                        </Item>
-
-                                        <Item floatingLabel>
-                                            <Icon type="Entypo" name="location-pin"
-                                                  style={{fontSize: 15, color: '#303B43'}}/>
-                                            <Label style={{color: '#303B43', fontSize: 10}}>Venue</Label>
-                                            <Input
-                                                style={{fontSize: 15}}
-                                                onChangeText={(venue) => this.setState({venue})}
-
-                                            />
-                                        </Item>
-
-
-                                    </Form>
-
-                                    <View style={styles.modalButton}>
-                                        <Button rounded style={styles.button} onPress={this.confirm_order}>
-                                            <Text style={{
-                                                textAlign: 'center',
-                                                fontWeight: '200',
-                                                color: 'white',
-                                                fontSize: 13
-                                            }}>Add Event</Text>
-
-                                        </Button>
-                                    </View>
-
-                                </View>
-
-
-                            </View>
-
-                        </Content>
-
-                    </Modal>
 
                     <Tabs>
                         <Tab heading={<TabHeading><Text>Event</Text></TabHeading>}>
@@ -493,14 +412,6 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: 5,
     },
-    modalButton:{
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 30,
-        width: 100
 
-
-
-    }
 
 });
