520e36aab1ef6dccd789dbd19b63e4aab3327273
maryanne <mary.jereh@gmail.com>
2019-06-03 16:52:14 +0300

    clean up created a clientdetails tab in components to display the client details

--- screens/EventDetailsScreen.js
+++ screens/EventDetailsScreen.js
@@ -15,7 +15,7 @@ import {
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import {store} from '../Redux/store';
 import Geocoder from 'react-native-geocoding';
-
+import ClientDetailsTab from '../components/ClientDetailsTab';
 import EventDetailsTab from '../components/EventDetailsTab';
 
 const _ = require('lodash');
@@ -100,58 +100,7 @@ export default class EventDetailsScreen extends React.Component {
 
                         </Tab>
                         <Tab heading={<TabHeading><Text>Client</Text></TabHeading>}>
-                            <View style={{paddingRight: 10, paddingLeft: 10}}>
-                                <Text>{""}</Text>
-
-                                <Text style={styles.font}>
-                                    {this.state.client.name}
-                                </Text>
-                                <Text>{" "}</Text>
-
-                                <ListItem icon noBorder>
-                                    <Left>
-                                        <Icon type="Ionicons" name="ios-people"/>
-                                    </Left>
-                                    <Body>
-                                        <Text style={{fontWeight: '200', fontSize: 13}}>{this.state.client.name}</Text>
-                                        <Text
-                                            style={{fontSize: 13, fontWeight: '200'}}> {this.state.client.email}</Text>
-                                        <Text style={{fontSize: 13, fontWeight: '200'}}>{this.state.client.phone}</Text>
-
-                                    </Body>
-
-                                </ListItem>
-                                <Text>{" "}</Text>
-
-                                <ListItem icon noBorder>
-                                    <Left>
-                                        <Icon type="MaterialIcons" name="event-note"
-                                              style={{color: '#303B43'}}/>
-                                    </Left>
-                                    <Body>
-                                        <Text style={{fontWeight: '200', fontSize: 12}} note>Client Notes</Text>
-                                        <Text style={{fontSize: 13, fontWeight: '200'}}>{this.state.client.notes}</Text>
-                                        <Text style={{fontSize: 12, color: '#00adf5'}}>Read More...</Text>
-
-                                    </Body>
-                                </ListItem>
-                                <Text>{" "}</Text>
-                                <ListItem icon noBorder>
-                                    <Left>
-                                        <Icon type="MaterialCommunityIcons" name="web"
-                                              style={{color: '#303B43'}}/>
-                                    </Left>
-                                    <Body>
-                                        <Text style={{fontWeight: '200', fontSize: 12}} note>Web site</Text>
-                                        <Text style={{fontSize: 12, fontWeight: '200'}}>Check out the client
-                                            website</Text>
-                                        <Text style={{fontSize: 12, color: '#00adf5'}}
-                                              onPress={() => Linking.openURL(this.state.client.website)}>view
-                                            ....</Text>
-
-                                    </Body>
-                                </ListItem>
-                            </View>
+                            <ClientDetailsTab {...this.state}/>
                         </Tab>
                         <Tab heading={<TabHeading><Text>Venue</Text></TabHeading>}>
                             <View style={{paddingRight: 10, paddingLeft: 10}}>
@@ -256,11 +205,6 @@ const styles = StyleSheet.create({
     container: {
         justifyContent: 'space-between'
     },
-    font:
-        {
-            fontWeight: 'bold',
-            fontSize: 30,
-        },
     card: {
         width: 300,
         height: 100,
