c78ad31065aeed82f74c9def40a7f8809d9dca4b
maryanne <mary.jereh@gmail.com>
2019-06-10 17:24:08 +0300

    added a styled component to the title

--- src/components/eventComponents/HeaderComponent.js
+++ src/components/eventComponents/HeaderComponent.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import {Body,Header, Icon, Left, Right, Text} from "native-base";
+import {Body,Header, Icon, Left, Right} from "native-base";
+import {Title} from '../styledComponents/events/events'
+
 export default class HeaderComponent extends React.Component {
     render() {
         return (
-
-            <Header style={{backgroundColor: '#0066ff'}}>
+            <Header style={{backgroundColor:'#0066ff'}}>
                 <Left/>
                 <Body>
-                    <Text style={{fontWeight: '200', color: 'white'}}>Events</Text>
+                    <Title style={{fontWeight: '200', color: 'white'}}>Events</Title>
                 </Body>
                 <Right>
                     <Icon type="Entypo" name="user" style={{color: 'white', fontSize: 25}}
                           onPress={this.openActionSheet}/>
                 </Right>
-
             </Header>
-
-
         )
     }
 }
--- src/components/styledComponents/events/events.js
+++ src/components/styledComponents/events/events.js
@@ -2,3 +2,8 @@ import styled from 'styled-components';
 export const   Header = styled.View `
  background-color: #0066ff
     `;
+
+export const Title = styled.Text`
+    color: white;
+    font-weight: 200
+`;
