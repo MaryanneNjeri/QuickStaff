f6be476ef7c02854e965220e12c6ed88348910a8
maryanne <mary.jereh@gmail.com>
2019-06-13 14:13:01 +0300

    fixed eslint issues

--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -7,12 +7,12 @@ import {
 } from 'native-base';
 import { LinearGradient } from 'expo';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { validateInput } from '../../components/lib/functions/auth/validateInput';
 import { login } from '../../redux/login/action';
 import LoginForm from '../../components/login/LoginForm';
 import Loader from '../../components/general/Loader';
 
-const _ = require('lodash');
 
 class LoginScreen extends Component {
   constructor(props) {
@@ -30,66 +30,69 @@ class LoginScreen extends Component {
       this.props.navigation.navigate('PasswordReset');
     };
 
-    signIn = async (email, password) => {
-      const user = {
-        email,
-        password,
-      };
-      const { errors, isValid } = validateInput(user);
-      if (!isValid) {
-        this.setState({
-          errors,
-        });
-      } else if (isValid) {
-        this.setState({ errors: {}, loading: true });
-        this.props.dispatch(login(user));
-        AsyncStorage.getItem('token').then((data) => {
-          if (data) {
-            Toast.show({
-              text: ' Successfully Log in',
-              type: 'success',
-              position: 'top',
-              duration: 3000,
-            });
-            this.props.navigation.navigate('App');
-          } else {
-            Toast.show({
-              text: 'Invalid login credentials',
-              type: 'danger',
-              position: 'top',
-              duration: 3000,
-
-            });
-            this.props.navigation.navigate('Auth');
-          }
-        });
-      }
+  signIn = async (email, password) => {
+    const user = {
+      email,
+      password,
     };
+    const { errors, isValid } = validateInput(user);
+    if (!isValid) {
+      this.setState({
+        errors,
+      });
+    } else if (isValid) {
+      this.setState({ errors: {}, loading: true });
+      const { dispatch } = this.props;
+      dispatch(login(user));
+      AsyncStorage.getItem('token').then((data) => {
+        if (data) {
+          Toast.show({
+            text: ' Successfully Log in',
+            type: 'success',
+            position: 'top',
+            duration: 3000,
+          });
 
-    render() {
-      const { loading } = this.props;
-      if (loading) {
-        return (
-          <Loader />
-        );
-      }
+          this.props.navigation.navigate('App');
+        } else {
+          Toast.show({
+            text: 'Invalid login credentials',
+            type: 'danger',
+            position: 'top',
+            duration: 3000,
+
+          });
+
+          this.props.navigation.navigate('Auth');
+        }
+      });
+    }
+  };
+
+  render() {
+    const { loading } = this.props;
+    if (loading) {
       return (
-        <Container>
-          <LinearGradient
-            colors={['#ff6600', '#ff6699']}
-            style={styles.container}
-          >
-            <Content contentContainerStyle={{ justifyContent: 'center', flex: 1 }}>
-              <View style={styles.loginContainer}>
-                <Image resizeMode="contain" style={styles.logo} source={require('../../../assets/images/logo.png')} />
-              </View>
-
-              <LoginForm {...this.state} signIn={this.signIn} onRedirect={this.onRedirect} />
-            </Content>
-          </LinearGradient>
-        </Container>
+        <Loader />
       );
     }
+    return (
+      <Container>
+        <LinearGradient
+          colors={['#ff6600', '#ff6699']}
+          style={styles.container}
+        >
+          <Content contentContainerStyle={{ justifyContent: 'center', flex: 1 }}>
+            <View style={styles.loginContainer}>
+              <Image resizeMode="contain" style={styles.logo} source={require('../../../assets/images/logo.png')} />
+            </View>
+
+            <LoginForm {...this.state} signIn={this.signIn} onRedirect={this.onRedirect} />
+          </Content>
+        </LinearGradient>
+      </Container>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -109,6 +112,11 @@ const styles = StyleSheet.create({
   },
 
 });
+
+LoginScreen.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  loading: PropTypes.func.isRequired,
+};
 // we define the props
 const mapStateToProps = state => ({
   loading: state.token.loading,
