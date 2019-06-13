257aa82c75510e2bb20c418868640fb7cdfb9785
maryanne <mary.jereh@gmail.com>
2019-06-13 12:26:44 +0300

    added a splash.png

--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -1,112 +1,117 @@
-import React, {Component} from 'react';
-import {View,StyleSheet, Image, AsyncStorage} from 'react-native';
-import {Content, Container,Toast, Spinner} from "native-base";
-import {validateInput} from "../../components/lib/functions/auth/validateInput";
-import {LinearGradient} from 'expo';
-import {connect} from 'react-redux';
-import {login} from '../../redux/login/action';
-import LoginForm from '../../components/login/LoginForm'
+import React, { Component } from 'react';
+import {
+  View, StyleSheet, Image, AsyncStorage,
+} from 'react-native';
+import {
+  Content, Container, Toast,
+} from 'native-base';
+import { LinearGradient } from 'expo';
+import { connect } from 'react-redux';
+import { validateInput } from '../../components/lib/functions/auth/validateInput';
+import { login } from '../../redux/login/action';
+import LoginForm from '../../components/login/LoginForm';
 import Loader from '../../components/general/Loader';
+
 const _ = require('lodash');
 
 class LoginScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email:'',
-            password:'',
-            errors: {},
-            token: '',
-            loading: false
-        }
-    }
-    onRedirect = () => {
-        this.props.navigation.navigate('PasswordReset')
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      errors: {},
+      token: '',
+      loading: false,
     };
-    signIn = async (email,password) => {
-        let user = {
-            email: email,
-            password: password
-        };
-        const{ errors, isValid}=validateInput(user);
-        if (!isValid) {
-            this.setState({
-                errors
-            })
-        }
-        else if(isValid) {
-            this.setState({errors: {}, loading: true});
-            this.props.dispatch(login(user));
-            AsyncStorage.getItem('token').then((data) => {
-                if (data) {
-                    Toast.show({
-                        text: " Successfully Log in",
-                        type: "success",
-                        position: "top",
-                        duration: 3000
-                    });
-                    this.props.navigation.navigate('App')
-                } else {
-                    Toast.show({
-                        text: "Invalid login credentials",
-                        type: "danger",
-                        position: "top",
-                        duration: 3000
+  }
 
-                    });
-                    this.props.navigation.navigate('Auth');
-                }
+    onRedirect = () => {
+      this.props.navigation.navigate('PasswordReset');
+    };
+
+    signIn = async (email, password) => {
+      const user = {
+        email,
+        password,
+      };
+      const { errors, isValid } = validateInput(user);
+      if (!isValid) {
+        this.setState({
+          errors,
+        });
+      } else if (isValid) {
+        this.setState({ errors: {}, loading: true });
+        this.props.dispatch(login(user));
+        AsyncStorage.getItem('token').then((data) => {
+          if (data) {
+            Toast.show({
+              text: ' Successfully Log in',
+              type: 'success',
+              position: 'top',
+              duration: 3000,
             });
-        }
+            this.props.navigation.navigate('App');
+          } else {
+            Toast.show({
+              text: 'Invalid login credentials',
+              type: 'danger',
+              position: 'top',
+              duration: 3000,
 
+            });
+            this.props.navigation.navigate('Auth');
+          }
+        });
+      }
     };
 
     render() {
-        const {loading} = this.props;
-        if (loading) {
-            return (
-               <Loader/>
-            )
-        }
+      const { loading } = this.props;
+      if (loading) {
         return (
-            <Container>
-                <LinearGradient
-                    colors={['#ff6600', '#ff6699']}
-                    style={styles.container}>
-                    <Content contentContainerStyle={{justifyContent: 'center', flex: 1}}>
-                        <View style={styles.loginContainer}>
-                            <Image resizeMode="contain" style={styles.logo} source={require('../../../assets/images/logo.png')}/>
-                        </View>
+          <Loader />
+        );
+      }
+      return (
+        <Container>
+          <LinearGradient
+            colors={['#ff6600', '#ff6699']}
+            style={styles.container}
+          >
+            <Content contentContainerStyle={{ justifyContent: 'center', flex: 1 }}>
+              <View style={styles.loginContainer}>
+                <Image resizeMode="contain" style={styles.logo} source={require('../../../assets/images/logo.png')} />
+              </View>
 
-                        <LoginForm {...this.state} signIn={this.signIn} onRedirect={this.onRedirect}/>
-                    </Content>
-                </LinearGradient>
-            </Container>
-        )
+              <LoginForm {...this.state} signIn={this.signIn} onRedirect={this.onRedirect} />
+            </Content>
+          </LinearGradient>
+        </Container>
+      );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 15
-    },
-    loginContainer: {
-        alignItems: 'center',
-        flexGrow: 0.5,
-        justifyContent: 'center'
-    },
-    logo: {
-        position: 'absolute',
-        width: 300,
-        height: 100
-    },
+  container: {
+    flex: 1,
+    padding: 15,
+  },
+  loginContainer: {
+    alignItems: 'center',
+    flexGrow: 0.5,
+    justifyContent: 'center',
+  },
+  logo: {
+    position: 'absolute',
+    width: 300,
+    height: 100,
+  },
 
 });
 // we define the props
 const mapStateToProps = state => ({
-    loading: state.token.loading,
-    error: state.token.Error,
+  loading: state.token.loading,
+  error: state.token.Error,
 });
-export default connect(mapStateToProps)(LoginScreen)
-
+export default connect(mapStateToProps)(LoginScreen);
--- src/screens/auth/PasswordResetScreen.js
+++ src/screens/auth/PasswordResetScreen.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View ,TextInput,Dimensions,Image,TouchableHighlight} from 'react-native';
-import { LinearGradient,Font } from 'expo';
+import {
+  StyleSheet, Text, View, TextInput, Dimensions, Image, TouchableHighlight,
+} from 'react-native';
+import { LinearGradient, Font } from 'expo';
 
 const { height, width } = Dimensions.get('window');
 export default class PasswordResetScreen extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {oldpassword: ''};
-        this.state = {newpassword:''};
-      }
+  constructor(props) {
+    super(props);
+    this.state = { oldpassword: '' };
+    this.state = { newpassword: '' };
+  }
+
       state = {
         fontLoaded: false,
       };
@@ -16,124 +19,129 @@ export default class PasswordResetScreen extends React.Component {
       // reauthenticate = () =>{
 
       // }
-      resetPassword = () =>{
+      resetPassword = () => {
         // this.reauthenticate
-        this.props.navigation.navigate('Event')
-    };
-
-      async componentDidMount(){
-         await Font.loadAsync({'shadows-into-light':require('../../../assets/fonts/ShadowsIntoLight.ttf')
-        });
-        this.setState({ fontLoaded: true });
+        this.props.navigation.navigate('Event');
       };
-      
-  render() {
-    let icon = {
-        url:"https://img.icons8.com/clouds/400/000000/lock-2.png"
-    }
-    return ( 
-        <LinearGradient
-        colors={['#0066ff','#0033cc']}
-        style={styles.container}>
 
-      <View style={styles.container}>
-      <View style={styles.card}>
-         <Image  resizeMode="contain" source={icon} style={styles.logo}/>
-       </View> 
-               
-        { 
+      async componentDidMount() {
+        await Font.loadAsync({ 'shadows-into-light': require('../../../assets/fonts/ShadowsIntoLight.ttf') });
+        this.setState({ fontLoaded: true });
+      }
+
+      render() {
+        const icon = {
+          url: 'https://img.icons8.com/clouds/400/000000/lock-2.png',
+        };
+        return (
+          <LinearGradient
+            colors={['#0066ff', '#0033cc']}
+            style={styles.container}
+          >
+
+            <View style={styles.container}>
+              <View style={styles.card}>
+                <Image resizeMode="contain" source={icon} style={styles.logo} />
+              </View>
+
+              {
             this.state.fontLoaded ? (
-                <View style={styles.header}>
-          <Text style={styles.text}>Reset Your Password ?</Text> 
-          
-          
-        </View>
-            ) :null
+              <View style={styles.header}>
+                <Text style={styles.text}>Reset Your Password ?</Text>
+
+
+              </View>
+            ) : null
         }
-          
-         
-         <TextInput style= {styles.input}
-              returnKeyType="done" 
-              ref={(input)=> this.passwordInput = input} 
-              placeholder='Current Password' 
-              placeholderTextColor='rgba(225,225,225,0.7)'
-              onChangeText={(oldpassword) => this.setState({oldpassword})}
-              secureTextEntry/> 
-        
-        <TextInput style= {styles.input}
-              returnKeyType="done" 
-              ref={(input)=> this.passwordInput = input} 
-              placeholder='New Password' 
-              placeholderTextColor='rgba(225,225,225,0.7)'
-              onChangeText={(newpassword) => this.setState({newpassword})}
-              secureTextEntry/>   
-         <TouchableHighlight style={styles.buttonContainer} 
-                  onPress ={ () => this.resetPassword(this.state.oldpassword,this.state.newpassword)}>
-             <Text  style={styles.buttonText}>Reset your password</Text>
-</TouchableHighlight>   
-              
-         
-         </View>
-        
-     
-      </LinearGradient>
-    )
-      
-  }
+
+
+              <TextInput
+                style={styles.input}
+                returnKeyType="done"
+                ref={input => this.passwordInput = input}
+                placeholder="Current Password"
+                placeholderTextColor="rgba(225,225,225,0.7)"
+                onChangeText={oldpassword => this.setState({ oldpassword })}
+                secureTextEntry
+              />
+
+              <TextInput
+                style={styles.input}
+                returnKeyType="done"
+                ref={input => this.passwordInput = input}
+                placeholder="New Password"
+                placeholderTextColor="rgba(225,225,225,0.7)"
+                onChangeText={newpassword => this.setState({ newpassword })}
+                secureTextEntry
+              />
+              <TouchableHighlight
+                style={styles.buttonContainer}
+                onPress={() => this.resetPassword(this.state.oldpassword, this.state.newpassword)}
+              >
+                <Text style={styles.buttonText}>Reset your password</Text>
+              </TouchableHighlight>
+
+
+            </View>
+
+
+          </LinearGradient>
+        );
+      }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding:15,
-    
+    padding: 15,
+
   },
- text:{
+  text: {
     color: '#fff',
     fontSize: 20,
-    fontFamily:'shadows-into-light'
- },
+    fontFamily: 'shadows-into-light',
+  },
   header: {
     alignItems: 'center',
-    paddingBottom: 20
-   
+    paddingBottom: 20,
+
   },
   logo: {
     position: 'absolute',
     width: 400,
-    height: 150
-},
-  card:{
+    height: 150,
+  },
+  card: {
     alignItems: 'center',
     flexGrow: 0.5,
     justifyContent: 'center',
-    marginBottom:20
-   
+    marginBottom: 20,
+
   },
-  input :{
-    height:40,
-    backgroundColor: 'rgba(225,225,225,0.2)', 
-    marginBottom:20,
-    padding:10,
+  input: {
+    height: 40,
+    backgroundColor: 'rgba(225,225,225,0.2)',
+    marginBottom: 20,
+    padding: 10,
     color: '#fff',
-    borderRadius:30,
-    
+    borderRadius: 30,
+
   },
-  buttonText:{
+  buttonText: {
     color: '#fff',
     textAlign: 'center',
-   
-},
-  buttonContainer:{
-    height:45,
+
+  },
+  buttonContainer: {
+    height: 45,
     flexDirection: 'row',
     justifyContent: 'center',
-    borderRadius: 30 ,
+    borderRadius: 30,
     marginBottom: 20,
     backgroundColor: '#9ACD32',
-    paddingVertical: 15 ,
-   
-   
-},
+    paddingVertical: 15,
+
+
+  },
 
 });
