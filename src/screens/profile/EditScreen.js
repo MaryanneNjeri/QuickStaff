730e0fab0019468d70987aa664eec1cbf99c0f7c
maryanne <mary.jereh@gmail.com>
2019-06-18 21:45:54 +0300

    switiching to edit screen

--- src/screens/profile/EditScreen.js
+++ src/screens/profile/EditScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Content, Container ,Text} from 'native-base';
+import { Content, Container, Text } from 'native-base';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class EditScreen extends React.Component {
   render() {
     return (
       <Container>
-        <Content>
+        <Content contentContainerStyle={{ flex: 1, padding: 15 }}>
           <Text>Edit Screen</Text>
+
         </Content>
       </Container>
     );
