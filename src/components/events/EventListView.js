3f5ac00fccb776b94e89a5015e20ae48f51a1992
maryanne <mary.jereh@gmail.com>
2019-06-19 20:24:22 +0300

    displaying event list card showing the date and how long before the event date

--- src/components/events/EventListView.js
+++ src/components/events/EventListView.js
@@ -2,8 +2,8 @@ import React from 'react';
 import {
   Body, Icon, List, ListItem, Right, Text, View,
 } from 'native-base';
+import { TouchableHighlight } from 'react-native';
 import EventListCard from './EventsListCard';
-
 // eslint-disable-next-line react/prefer-stateless-function
 export default class EventListView extends React.Component {
   constructor() {
@@ -17,10 +17,18 @@ export default class EventListView extends React.Component {
     this.setState({
       isVisible: true,
     });
-  }
+  };
+
+  closeCard=() => {
+    this.setState({
+      isVisible: false,
+    });
+  };
+
 
   render() {
     const { assign, i, eventDetails } = this.props;
+    const { isVisible } = this.state;
 
 
     return (
@@ -31,19 +39,19 @@ export default class EventListView extends React.Component {
             <Body>
               <Text style={{ fontWeight: '200' }}>{assign.task.shift.event.name}</Text>
               <Text note>{Date(assign.task.shift.event.starts_at)}</Text>
-              <Icon
-                name="down"
-                type="AntDesign"
-                style={{ fontSize: 15, color: '#303B43' }}
-                onPress={this.viewListCard}
-              />
+              <Text>{' '}</Text>
+              <TouchableHighlight onPress={this.viewListCard}>
+                <Text note style={{ fontSize: 10 }}>
+view in calendar
+                </Text>
+              </TouchableHighlight>
             </Body>
             <Right>
               <Icon active name="arrow-forward" />
             </Right>
           </ListItem>
         </List>
-        { this.state.isVisible ? <EventListCard /> : null}
+        { isVisible ? <EventListCard closeCard={this.closeCard} event={assign.task.shift.event} /> : null}
 
       </View>
     );
--- src/components/events/EventsListCard.js
+++ src/components/events/EventsListCard.js
@@ -1,12 +1,100 @@
 import React from 'react';
-import { Text, View } from 'native-base';
+import {
+  Icon, Text, View, Card, CardItem, Left, Body, Right,
+} from 'native-base';
+import { TouchableHighlight, StyleSheet, Dimensions } from 'react-native';
+import moment from 'moment/moment';
+
+const { width } = Dimensions.get('window');
+// eslint-disable-next-line react/prefer-stateless-function
+const styles = StyleSheet.create({
+  card: {
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+    height: width / 3.5,
+    borderLeftWidth: 5,
+    borderLeftColor: 'tomato',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+    elevation: 6,
+  },
+
+  left: {
+    paddingRight: 10,
+    paddingLeft: 10,
+    borderLeftWidth: 0.3,
+    borderLeftColor: '#BDC3C7',
+  },
+  background: {
+    backgroundColor: '#F8F8F8',
+    padding: 15,
+
+  },
+});
 
 // eslint-disable-next-line react/prefer-stateless-function
-export default class Error extends React.Component {
+export default class EventsListCard extends React.Component {
   render() {
+    const { closeCard, event } = this.props;
     return (
-      <View>
-        <Text>Events Card</Text>
+      <View style={styles.background}>
+        <TouchableHighlight
+          style={{ alignSelf: 'flex-end' }}
+          onPress={() => {
+            closeCard();
+          }}
+        >
+          <Text style={{ fontWeight: '200', color: '#303B43', fontSize: 13 }}>
+            {' '}
+                  Close
+            <Icon
+              name="close"
+              type="EvilIcons"
+              style={{ fontSize: 20, color: '#303B43' }}
+            />
+          </Text>
+        </TouchableHighlight>
+
+        <Card style={styles.card}>
+          <CardItem>
+            <Left>
+              <Body>
+                <Text style={{ fontWeight: 'bold', fontSize: 24, color: '#0033cc' }}>{moment(event.starts_at).format('D')}</Text>
+                <Text style={{ fontWeight: '200', fontSize: 20, color: '#0033cc' }}>{moment(event.starts_at).format('MMM')}</Text>
+              </Body>
+            </Left>
+            <Body style={styles.left}>
+              <Text style={{ fontSize: 15, fontWeight: '200' }}>{event.name}</Text>
+              <Text note style={{ fontWeight: '200', fontSize: 12 }}>
+Client
+                {event.client.name}
+              </Text>
+              <Text>{' '}</Text>
+              <Text note style={{ fontWeight: '200', fontSize: 12 }}>
+                <Icon
+                  type="Entypo"
+                  name="location-pin"
+                  style={{ color: '#303B43', fontSize: 10 }}
+                />
+                <Text>{' '}</Text>
+                {event.venue.name}
+              </Text>
+
+            </Body>
+            <Right>
+              <Text note style={{ fontSize: 10 }}>
+                {' '}
+                {moment(event.starts_at).fromNow()}
+              </Text>
+            </Right>
+          </CardItem>
+        </Card>
       </View>
     );
   }
