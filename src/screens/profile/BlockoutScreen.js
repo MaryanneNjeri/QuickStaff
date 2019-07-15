e54d36fbbd4a6040bfd1e72c1b26c095091561fd
maryanne <mary.jereh@gmail.com>
2019-07-15 14:14:14 +0300

    allow user to search for a blockout

--- src/screens/profile/BlockoutScreen.js
+++ src/screens/profile/BlockoutScreen.js
@@ -1,81 +1,154 @@
 import React from 'react';
 import {
-  Body, Container, Content, Spinner, Text, View, List, ListItem,
+  Body, Container, Content, Spinner, Text, View, List, ListItem, Icon,
 } from 'native-base';
-import { TouchableHighlight } from 'react-native';
+import { TouchableHighlight, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import moment from 'moment/moment';
 import _ from 'lodash';
 import PropTypes from 'prop-types';
 import { fetchBlockouts } from '../../redux/blockouts/action';
+import FormInput from '../../components/common/controls/Form/FormInput';
 
 class BlockoutScreen extends React.Component {
-  componentDidMount() {
-    this.props.dispatch(fetchBlockouts());
+  constructor() {
+    super();
+    this.state = {
+      visible: false,
+      filtered: [],
+    };
   }
 
-  render() {
-    const { error, loading, blockouts } = this.props;
+  componentDidMount() {
+    const { dispatch, blockouts } = this.props;
+    dispatch(fetchBlockouts());
+    this.setState({
+      filtered: blockouts.data,
+    });
+  }
 
+  componentWillReceiveProps(nextProps, nextContext) {
+    this.setState({
+      filtered: nextProps.blockouts.data,
+    });
+  }
 
-    if (error) {
-      return (
+    closeSearch = () => {
+      this.setState({ visible: false });
+    };
 
-        <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-          <Text>
-            {' '}
+    searchEvent = () => {
+      this.setState({ visible: true });
+    };
+
+   handleChange=(e) => {
+     const { blockouts } = this.props;
+     let currentList = [];
+     let newList = [];
+     // if the search bar isn't empty
+     if (e !== '') {
+       currentList = blockouts.data;
+       newList = currentList.filter((block) => {
+         const lc = block.reason.toLowerCase();
+
+         // change also the search term to lower case
+         const filter = e.toLowerCase();
+         return lc.includes(filter);
+       });
+       console.log(newList);
+     } else {
+       newList = blockouts.data;
+     }
+     this.setState({
+       filtered: newList,
+     });
+   }
+
+   render() {
+     const { error, loading } = this.props;
+     const { visible, filtered } = this.state;
+
+     if (error) {
+       return (
+
+         <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+           <Text>
+             {' '}
 An error occurred!
-            {error.message}
-          </Text>
-        </View>
-      );
-    }
-    if (loading) {
-      return (
-        <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-          <Spinner style={{ height: 80 }} size="large" color="tomato" />
+             {error.message}
+           </Text>
+         </View>
+       );
+     }
+     if (loading) {
+       return (
+         <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+           <Spinner style={{ height: 80 }} size="large" color="tomato" />
 
-        </View>
-      );
-    }
-    return (
-      <Container>
-        <Content>
-          {_.map(blockouts.data, (blockout, i) => (
+         </View>
+       );
+     }
+     return (
+       <Container>
+         <Content contentContainerStyle={{ padding: 15 }}>
+           <Icon name="search" onPress={this.searchEvent} type="Feather" style={{ fontSize: 20, color: '#0052cc' }} />
 
-            <List key={i}>
-              <ListItem>
-                <Body>
-                  <Text style={{ fontWeight: '200', fontSize: 16 }}>{blockout.recurrence_text}</Text>
-                  <Text style={{ fontWeight: '200', fontSize: 14 }} note>
+           {visible ? (
+             <View style={{ padding: 15 }}>
+               <TouchableOpacity onPress={this.closeSearch} style={{ alignSelf: 'flex-end' }}>
+                 <Text style={{ fontSize: 15, fontWeight: '200', color: '#0052cc' }}>
+                            Close
+                   <Icon
+                     name="close"
+                     type="EvilIcons"
+                     style={{ fontSize: 15, color: '#0052cc' }}
+                   />
+                 </Text>
+               </TouchableOpacity>
+               <FormInput
+                 standard
+                 label="Search for blockouts reasons"
+                 floatingLabel
+                 onChangeText={e => this.handleChange(e)}
+               />
+             </View>
+           ) : null}
+           {
+                _.map(filtered, (blockout, i) => (
+
+                  <List key={i}>
+                    <ListItem>
+                      <Body>
+                        <Text style={{ fontWeight: '200', fontSize: 16 }}>{blockout.recurrence_text}</Text>
+                        <Text style={{ fontWeight: '200', fontSize: 14 }} note>
                       Starts at:
-                    {' '}
-                    {' '}
-                    {moment(blockout.starts_at).format('LLL')}
-                  </Text>
-                  <Text style={{ fontWeight: '200', fontSize: 14 }} note>
+                          {' '}
+                          {' '}
+                          {moment(blockout.starts_at).format('LLL')}
+                        </Text>
+                        <Text style={{ fontWeight: '200', fontSize: 14 }} note>
                       Ends at:
-                    {' '}
-                    {' '}
-                    {moment(blockout.ends_at).format('LLL')}
-                  </Text>
-                  <TouchableHighlight onPress={this.viewDetails}>
-                    <Text note style={{ fontSize: 13 }}>
+                          {' '}
+                          {' '}
+                          {moment(blockout.ends_at).format('LLL')}
+                        </Text>
+                        <TouchableHighlight onPress={this.viewDetails}>
+                          <Text note style={{ fontSize: 13 }}>
                           Reason :
 
-                      {blockout.reason}
-                    </Text>
-                  </TouchableHighlight>
-                </Body>
-              </ListItem>
-            </List>
+                            {blockout.reason}
+                          </Text>
+                        </TouchableHighlight>
+                      </Body>
+                    </ListItem>
+                  </List>
 
-          ))}
+                ))}
 
-        </Content>
-      </Container>
-    );
-  }
+         </Content>
+       </Container>
+     );
+   }
 }
 
 const mapStateToProps = state => ({
